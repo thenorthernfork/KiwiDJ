2db6f73f0dd5267df4971d9baa2a41be913961df
medsouz <medsouz99@gmail.com>
2013-07-13 16:16:52 -0400

    Made the stats page look slightly better

--- routes/stats.js
+++ routes/stats.js
@@ -3,14 +3,20 @@ function getHeapData(){
 	var total = (process.memoryUsage().heapTotal/1000000).toString().split(".")
 	used = used[0]+"."+used[1].substring(0,2)+"MB";
 	total = total[0]+"."+total[1].substring(0,2)+"MB";
-	return used+"/"+total
+	return [used+"/"+total, (process.memoryUsage().heapUsed/process.memoryUsage().heapTotal) * 100];
 }
 
 //TODO: format to hours, minutes, and seconds
 
 function uptime(){
 	var uptime = process.uptime().toString().split(".");
-	return uptime[0]+"."+uptime[1].substring(0,2)+" seconds";
+	var seconds = uptime[0]
+	var numdays = Math.floor(seconds / 86400);
+	var numhours = Math.floor((seconds % 86400) / 3600);
+	var numminutes = Math.floor(((seconds % 86400) % 3600) / 60);
+	var numseconds = ((seconds % 86400) % 3600) % 60;
+
+	return numdays + " days, " + numhours + " hours, " + numminutes + " minutes, " + numseconds + " seconds";
 }
 
 exports.view = function(req, res){
