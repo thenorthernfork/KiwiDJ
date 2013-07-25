de9c12e37f49801cbac111fb5907a66add690587
medsouz <medsouz99@gmail.com>
2013-07-25 11:46:20 -0400

    Using a better default address
    Allows connections from any ip address

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	secret: "",//Session Secret Message
 	proxy_socket_fix: false,//use io.set("transports", ["xhr-polling", "jsonp-polling"]); fixes error if behind a reverse proxy
-	address: "127.0.0.1",
+	address: "0.0.0.0",
 	port: 3000
 }
